c2c3e9af1379f557fe7a4b893e2dfb7b8dcb854a
sanjeeva reddy <sanjeeva202net@gmail.com>
2021-08-16 20:58:32 +0530

    first commit

---
+++ DemoTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DemoTest.Controllers
+{
+    [ApiController]
+    
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild"
+            //, "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger= null)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("[controller]/{summary}")]
+        public IEnumerable<WeatherForecast> Get(string summary)
+        {
+            var rng = new Random();
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+            var f = result.Where(x => x.Summary == summary).ToList();
+            return f;
+        }
+    }
+}
---
+++ DemoTestUnitTests/UnitTest1.cs
@@ -0,0 +1,52 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoTest.Controllers;
+using System.Collections.Generic;
+using DemoTest;
+
+namespace DemoTestUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WeatherForecast_List_Failed()
+        {
+            WeatherForecastController wc = new WeatherForecastController();
+            //Assert
+            var ct = wc.Get();
+            int c = ((DemoTest.WeatherForecast[])ct).Length;
+            int count = 4;
+
+            Assert.AreEqual(c,count, "Count not matched");
+
+            
+        }
+
+        [TestMethod]
+        public void WeatherForecast_List_Succeded()
+        {
+            WeatherForecastController wc = new WeatherForecastController();
+            //Assert
+            var ct = wc.Get();
+            int c = ((DemoTest.WeatherForecast[])ct).Length;
+            int count = 5;
+
+            Assert.AreEqual(c, count, "Count not matched");
+        }
+
+        [TestMethod]
+        public void WeatherForecast_List_SummaryCheck()
+        {
+            WeatherForecastController wc = new WeatherForecastController();
+            //Assert
+            string summary = "Mild";
+            var ct = wc.Get(summary);
+
+            int c = ((DemoTest.WeatherForecast[])ct).Length;
+
+            int expected = 1;
+
+             Assert.AreEqual(c, expected, "Count matched the records");
+        }
+    }
+}
